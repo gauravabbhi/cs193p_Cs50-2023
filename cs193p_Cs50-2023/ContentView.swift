854e64007e4263f39fdf3e6147c0ceebe7680556
Gaurav Abbhi <gaurav.abbhi@me.com>
2024-02-08 16:54:04 -0500

    Initial Commit

---
+++ cs193p_Cs50-2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cs193p_Cs50-2023
+//
+//  Created by Karan on 08/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cs193p_Cs50-2023/cs193p_Cs50_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  cs193p_Cs50_2023App.swift
+//  cs193p_Cs50-2023
+//
+//  Created by Karan on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct cs193p_Cs50_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
