62a0ea80ce4fe506bfa2807bd1e337f1073587ed
Gaurav Abbhi <gaurav.abbhi@me.com>
2024-02-10 21:23:51 -0500

    Array-fied card with ForEach

--- cs193p_Cs50-2023/ContentView.swift
+++ cs193p_Cs50-2023/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: [String] = ["⚔️", "🛡️", "🪖", "🏹"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
         }
         .padding()
+    }
+    
+}
+
+struct CardView : View {
+    @State var content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerSize: CGSize(width: 20, height: 10))
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill(.green)
+            }
+            
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+//            var generator = SystemRandomNumberGenerator()
+//            let emojis = ["⚔️", "🛡️", "🪖", "🏹", "🚀", "🔫", "💣", "🚁", "🛰️", "🚢", "🚀", "🚁", "🛰️", "🛸"]
+//            if let randomEmoji = emojis.randomElement(using: &generator) {
+//                self.content = randomEmoji
+//            } else {
+//                self.content = content
+//            }
+        }
     }
 }
 
